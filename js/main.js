ddc8889241e86b03f6b448e7f88676bac8afdd14
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-02 17:45:59 +0100

    no way xD (bonus)

--- js/main.js
+++ js/main.js
@@ -6,11 +6,17 @@ for (let i = 0; i < 100; i++) {
 
     if ((j % 3 == 0) && (j % 5 == 0)) {
         console.log("FizzBuzz");
+        square.innerHTML += `FizzBuzz`;
     } else if (j % 3 == 0) {
         console.log("Fizz");
+        square.innerHTML += `Fizz`;
     } else if (j % 5 == 0) {
         console.log("Buzz");
+        square.innerHTML += `Buzz`;
     } else {
         console.log(j);
+        square.innerHTML += `${j}`;
     }
+
+    square.innerHTML += `<div class="col my-col"></div>`;
 }
