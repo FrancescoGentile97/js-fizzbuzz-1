846c8c22f92d77e38473fbf7139f8778ce89e8b8
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-02 18:01:45 +0100

    Bug Fix

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 let j = 0;
+const square = document.getElementById ("square");
 
 for (let i = 0; i < 100; i++) {
 
     j = i + 1;
 
+    let valore = '';
+
     if ((j % 3 == 0) && (j % 5 == 0)) {
         console.log("FizzBuzz");
-        square.innerHTML += `FizzBuzz`;
+        valore = `FizzBuzz`;
     } else if (j % 3 == 0) {
         console.log("Fizz");
-        square.innerHTML += `Fizz`;
+        valore = `Fizz`;
     } else if (j % 5 == 0) {
         console.log("Buzz");
-        square.innerHTML += `Buzz`;
+        valore = `Buzz`;
     } else {
         console.log(j);
-        square.innerHTML += `${j}`;
+        valore = j;
     }
 
-    square.innerHTML += `<div class="col my-col"></div>`;
+    square.innerHTML += `<div class="col my-col">${valore}</div>`;
 }
