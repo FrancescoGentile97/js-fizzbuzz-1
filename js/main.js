0a10807734314a47d5965237d6b23638d49cad99
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-02 15:53:13 +0100

    gg ez

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-for (let i = 0; i <= 100; i++) {
-    if ((i % 3 === 0) && (i % 5 === 0)) {
+let j = 0;
+
+for (let i = 0; i < 100; i++) {
+
+    j = i + 1;
+
+    if ((j % 3 == 0) && (j % 5 == 0)) {
         console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
+    } else if (j % 3 == 0) {
         console.log("Fizz");
-    } else if (i % 5 === 0) {
+    } else if (j % 5 == 0) {
         console.log("Buzz");
     } else {
-        console.log(i);
+        console.log(j);
     }
 }
